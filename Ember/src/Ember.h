de558e4e32af2b56dd67b4ab6be4d15b73d816f3
Shakenbeet <luke.gobin1@icloud.com>
2021-06-22 20:36:43 -0500

    Add Core and Client Logging.

--- Ember/src/Ember.h
+++ Ember/src/Ember.h
@@ -3,6 +3,7 @@
 // For use in Ember Applications
 
 #include "Ember/Application.h"
+#include "Ember/Log.h"
 
 // ---Entry Point----------------------
 #include "Ember/EntryPoint.h"
--- Ember/src/Ember/EntryPoint.h
+++ Ember/src/Ember/EntryPoint.h
@@ -6,6 +6,11 @@ extern Ember::Application* Ember::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Ember::Log::Init();
+	EB_CORE_WARN("Initialized Log!");
+	int a = 5;
+	EB_INFO("Hello Var={0}", a);
+
 	auto app = Ember::CreateApplication();
 	app->Run();
 	delete app;
